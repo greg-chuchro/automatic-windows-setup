7a8aa2c7c0823af4e799c0d7818b2db729eb0f2a
greg-chuchro <ghbvft6@gmail.com>
2020-08-25 14:03:16 +0900

    fix winget installation

--- install_winget.ps1
+++ install_winget.ps1
@@ -6,5 +6,8 @@ $url = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
 $release = $webClient.DownloadString($url) | ConvertFrom-Json
 $fileName = $release.assets.browser_download_url -split '/' | Select-Object -Last 1
 
-$webClient.DownloadFile($release.assets.browser_download_url, ".\$fileName")
+$currentPath = (Get-Item .).FullName
+$webClient.DownloadFile($release.assets.browser_download_url, "$currentPath\$fileName")
 Add-AppxPackage -Path .\$fileName
+
+# FIXME https://docs.microsoft.com/en-us/windows/application-management/apps-in-windows-10
