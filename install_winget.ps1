c2637d4d41f0163596405a23aa56f12e58010dc4
greg-chuchro <gchuchro@acm.org>
2021-06-23 18:23:28 +0900

    mod winget installation

--- install_winget.ps1
+++ install_winget.ps1
@@ -1,13 +1,8 @@
-$webClient = New-Object System.Net.WebClient
-$webClient.Headers['User-Agent'] = 'AutomaticWindowsSetup'
+Start-Process 'ms-windows-store://pdp/?PFN=Microsoft.DesktopAppInstaller_8wekyb3d8bbwe'
 
-$url = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
-
-$release = $webClient.DownloadString($url) | ConvertFrom-Json
-$fileName = $release.assets.browser_download_url -split '/' | Select-Object -Last 1
-
-$currentPath = (Get-Item .).FullName
-$webClient.DownloadFile($release.assets.browser_download_url, "$currentPath\$fileName")
-Add-AppxPackage -Path .\$fileName
-
-# FIXME https://docs.microsoft.com/en-us/windows/application-management/apps-in-windows-10
+echo "Please, install App Installer (winget)"
+while ($true) {
+    if (Get-Command -ErrorAction Ignore -Type Application winget) { break }
+    Start-Sleep -Seconds 2
+echo "Please, install App Installer (winget)"
+}
